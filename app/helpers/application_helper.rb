6fe59e7748cd555ad189f61cb98e3dcc1d3f7181
murtazali.rasulow <murtazali.rasulow.05.@gmail.com>
2023-01-03 15:17:18 +0300

    Added count for questions, answers and unanswered questions

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,28 @@
+# module ApplicationHelper
+# 	def user_avatar(user)
+# 		# if user.avatar_url.present?
+# 		# 	user.avatar_url
+# 		# else
+# 		# 	asset_path 'avatar.jpg'
+# 		# end
+
+# 		return 'avatar.jpg'
+# 	end
+# end
+# В этом файле мы можем писать вспомогательные методы (хелперы) для
+# шаблонов (view) нашего приложения.
+module ApplicationHelper
+  # Этот метод возвращает ссылку на автарку пользователя, если она у него есть.
+  # Или ссылку на дефолтную аватарку, которая лежит в app/assets/images
+  def user_avatar(user)
+    if user.avatar_url.present?
+      user.avatar_url
+    else
+      asset_path 'avatar.jpg'
+    end
+  end
+
+  def fa_icon(icon_class)
+    content_tag 'span', '', class: "fa fa-#{icon_class}"
+  end
+end
